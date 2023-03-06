363ce8764decefd514993ed188a9f03c57b258fa
Sahil <sahilsapariya03@gmail.com>
2023-03-06 10:52:09 +0530

    created program for odd even number checker

---
+++ Bit-manipulation/even_or_odd.py
@@ -0,0 +1,25 @@
+"""
+    Find whether a number is EVEN or ODD
+        2 methods - % (modulo operator)
+                    & (bitwise AND operator)
+
+    eg. 20
+            1 0 1 0 0   == 20
+        &   0 0 0 0 1   == 1
+        -------------
+            0 0 0 0 0   == even     else odd
+"""
+
+def evenOddAnd(num):
+    if num & 1 == 1:
+        return f"{num} is odd"
+    return f"{num} is even"
+
+print(evenOddAnd(20))
+print(evenOddAnd(21))
+
+
+# output:
+
+# 20 is even
+# 21 is odd

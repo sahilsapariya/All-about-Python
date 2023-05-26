398b5a8d460eec1581fba66b6cc2cdf3953b631a
sahilsapariya <sahilsapariya03@gmail.com>
2023-05-26 19:35:17 +0530

    added more functionality to calculator

--- Desktop Applications/Tkinter tutorial/2_calculator.py
+++ Desktop Applications/Tkinter tutorial/2_calculator.py
@@ -1,6 +1,8 @@
 from tkinter import *
 
 root = Tk()
+root.title("Basic calculator")
+
 
 entry = Entry(root, width=35)
 entry.grid(row=0, column=0, columnspan=3, padx=5, pady=5)
@@ -17,13 +19,45 @@ def clear_number():
 def add_number():
     first_number = entry.get()
     global f_num 
+    global math 
+    math = "add"
+    if first_number.isdigit(): f_num = int(first_number)
+    entry.delete(0, END)
+
+def substract_number():
+    first_number = entry.get()
+    global f_num 
+    global math 
+    math = "substract"
+    if first_number.isdigit(): f_num = int(first_number)
+    entry.delete(0, END)
+
+def divide_number():
+    first_number = entry.get()
+    global f_num 
+    global math 
+    math = "divide"
+    if first_number.isdigit(): f_num = int(first_number)
+    entry.delete(0, END)
+
+def multiply_number():
+    first_number = entry.get()
+    global f_num 
+    global math 
+    math = "multiply"
     if first_number.isdigit(): f_num = int(first_number)
     entry.delete(0, END)
 
 def answer():
     second_number = entry.get()
     entry.delete(0, END)
-    entry.insert(0, int(f_num) + int(second_number))
+    
+    if math == "add": ans = int(f_num) + int(second_number)
+    elif math == "substract": ans = int(f_num) - int(second_number)
+    elif math == "divide": ans = int(f_num) / int(second_number)
+    elif math == "multiply": ans = int(f_num) * int(second_number)
+
+    entry.insert(0, str(ans))
 
 
 # Define buttons
@@ -39,6 +73,11 @@ button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: click_button
 button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: click_button(0))
 
 button_add = Button(root, text="+", padx=40, pady=20, command=lambda: add_number())
+button_substract = Button(root, text="-", padx=40, pady=20, command=lambda: substract_number())
+button_multiply = Button(root, text="x", padx=40, pady=20, command=lambda: multiply_number())
+button_divide = Button(root, text="/", padx=40, pady=20, command=lambda: divide_number())
+
+
 button_equal = Button(root, text="=", padx=85, pady=20, command=lambda: answer())
 button_clear = Button(root, text="clear", padx=75, pady=20, command=lambda: clear_number())
 
@@ -62,5 +101,8 @@ button_add.grid(row=5, column=0)
 button_clear.grid(row=4, column=1, columnspan=2)
 button_equal.grid(row=5, column=1, columnspan=2)
 
+button_substract.grid(row=6, column=0)
+button_divide.grid(row=6, column=1)
+button_multiply.grid(row=6, column=2)
 
 root.mainloop()

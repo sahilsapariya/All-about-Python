9cebfa16354770075813cf70fa7c1b344ff84e14
Sahil Sapariya <sahilsapariya03@gmail.com>
2024-10-13 10:42:31 +0530

    Complete: task1 and task2 completed for asyncio

---
+++ asyncio/task1/README.md
@@ -0,0 +1,24 @@
+## Part 1: Asynchronous HTTP Requests with aiohttp
+
+### Task 1. Basic Async HTTP Request Task
+
+__Task:__  Write a Python script that makes multiple asynchronous HTTP requests to fetch data from a public API (e.g., fetching details about GitHub repositories).
+
+
+__Details:__
+
+- Use aiohttp to make requests concurrently.
+- Ensure error handling for failed requests or timeouts.
+- Display the time taken to complete all requests.
+
+
+### Task 2. Parsing JSON Data Task
+
+__Task:__  Extend the previous task to parse the JSON response from the API and extract specific fields (e.g., repository name, stars, description).
+
+__Details:__
+
+- Use asyncio.gather to handle multiple requests at once.
+- Parse and format the results neatly.
+
+
---
+++ asyncio/task1/main.py
@@ -0,0 +1,67 @@
+import asyncio
+import aiohttp
+import time
+
+usernames = [
+    "octocat",
+    "sahilsapariya",
+    "PRANJALJAVIA",
+    "kevintamakuwala"
+]
+
+async def fetch_repo_names(username):
+    url = f"https://api.github.com/users/{username}/repos"
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                repos = await response.json()
+                repo_names = [repo['name'] for repo in repos]
+                return {
+                    "username": username,
+                    "repositories": repo_names
+                }
+            else:
+                return {
+                    "username": username,
+                    "error": response.status,
+                    "message": await response.json()
+                }
+
+async def main():
+    start_time = time.time()
+
+    # Create a list of asynchronous fetch tasks
+    users_repos = [fetch_repo_names(username) for username in usernames]
+    results = await asyncio.gather(*users_repos)
+
+    end_time = time.time()
+
+    print(results)
+    print(f"Time taken to complete this task is {(end_time - start_time):.2f} seconds.")
+
+
+asyncio.run(main())
+
+
+# OUTPUT:
+# [
+#     {
+#         'username': 'octocat', 
+#         'repositories': ['boysenberry-repo-1', 'git-consortium', 'hello-worId', 'Hello-World', 'linguist', 'octocat.github.io', 'Spoon-Knife', 'test-repo1']
+#     }, 
+#     {
+#         'username': 'sahilsapariya', 
+#         'repositories': ['8086-Programming', 'Advance-Fastag', 'All-About-Java', 'All-about-Python', 'Blockchain', 'Blog-Application', 'Clone-of-UK-government-website', 'cloud-expense', 'College360', 'crack-GRE', 'e-commerce-product', 'Elite-Mode', 'events', 'Flask-REST-API', 'hello-world', 'hostel-management', 'ML-Basics', 'movie_app_redux', 'mytodo', 'nirmaan-yaatra', 'Portfolio-sahil', 'product-visualizer', 'ranking_system', 'Role-based-Authentication', 'sahilsapariya', 'schoolweb', 'scrape-up', 'solar-system']
+#     }, 
+#     {
+#         'username': 'PRANJALJAVIA', 
+#         'repositories': ['Advance-Fastag', 'blood_bank_managment_system', 'Book-Store', 'Car-Rental-System', 'College-support_CE045_CE069_CE054_CE049', 'College360', 'Community-Website', 'Doctor_Appointment_Management_System', 'Expense-Tracker-System', 'flutter_external_device', 'GDSC_Task_1', 'GDSC_Task_2', 'ggd', 'myfirstrepo', 'PRANJALJAVIA', 'Quiz-Application', 'Resume-Builder', 'String_Calculator_TDD_Kata', 'Study_notes_sharing_website']
+#     }, 
+#     {
+#         'username': 'kevintamakuwala', 
+#         'repositories': ['Acehacks3.0-WiseNet', 'AJT-Labwork', 'Algorithms', 'App-Dev', 'ByteBattles', 'Competitive-Programming', 'Course-Management', 'CSES-Solutions', 'DUHACKS2.0', 'Duhacks3.0-AnubhavBaatein', 'FastTyping', 'FSD-Labwork', 'J2EE-Todo', 'Java-Backend', 'JDBC-CRUD', 'kevintamakuwala', 'Lets-Ship-Book-Reviews', 'LLD-Design-Patterns', 'Simple-Calculator', 'StayHomey', 'University-Permissions']
+#     }
+# ]
+
+# Time taken to complete this task is 1.29 seconds.
